6ab4a8d562668e5def68218292a3bbdb337a387b
Albert Shay <albertshay888@gmail.com>
2021-06-26 19:46:46 -0700

    color2

--- src/ListItem.js
+++ src/ListItem.js
@@ -10,9 +10,10 @@ const ListItem = () => {
     <div className='ListItem'>
       <img src={image} alt=''></img>
       <div className='name'>
-        <b>JOHN DOE</b>
+        <b>JOHN DOE</b> <b className='price'>$60</b>
       </div>
 
+      <div className='per'> per hour</div>
       <div className='itemInfo'>
         <div>
           <b>Experience: </b>
@@ -24,13 +25,16 @@ const ListItem = () => {
         </div>
         <div>
           <b>Availability: </b>
-          <span>Tu: 10 - 6pm / Th: 2 - 4pm</span>
+          <span>Tu: 10 - 6 pm</span>
+        </div>
+        <div>
+          <b className='hideLabel'>Availability: </b>
+          <span>Th: 2 - 4 pm</span>
         </div>
       </div>
 
-      <button className='price' onClick={() => setIsActive(!isActive)}>
-        <b>$60 </b>
-        <span>per hour</span>
+      <button className='button' onClick={() => setIsActive(!isActive)}>
+        BOOK LESSON
       </button>
 
       {isActive && <Email />}
